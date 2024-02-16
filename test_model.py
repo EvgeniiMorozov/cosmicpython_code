dda58de3f8cb7fa6141102bcb0e492f169486882
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2024-02-16 16:21:08 +0500

    Added test

--- test_model.py
+++ test_model.py
@@ -50,6 +50,12 @@ def test_can_only_deallocate_allocated_lines():
     assert batch.available_quantity == 20
 
 
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("ANGULAR-DESK", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
+
 def test_prefers_warehouse_batches_to_shipments():
     pytest.fail("todo")
 
