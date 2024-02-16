331f3b478fac5ee2b5dbdf95c90467fe1957d366
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2024-02-16 16:11:13 +0500

    Added tests

--- test_model.py
+++ test_model.py
@@ -13,7 +13,7 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
     line = OrderLine("order-ref", "SMALL-TABLE", 2)
     batch.allocate(line)
 
-    assert batch.availiable_quantity == 18
+    assert batch.available_quantity == 18
 
 
 def make_batch_and_line(sku, batch_qty, line_qty):
@@ -42,6 +42,12 @@ def test_cannot_allocate_if_skus_do_not_match():
     batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)
     different_sku = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
     assert batch.can_allocate(different_sku) is False
+
+
+def test_can_only_deallocate_allocated_lines():
+    batch, unallocated_line = make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 20
 
 
 def test_prefers_warehouse_batches_to_shipments():
